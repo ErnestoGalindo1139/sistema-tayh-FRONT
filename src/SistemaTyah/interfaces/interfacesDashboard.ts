52cecbad2fbcb22463aaccd2e35749b3e5956deb
Juan Carlos Rivas <juan_carlosr223@hotmail.com>
2025-02-18 19:28:41 -0700

    Se crea el Dashboard

---
+++ src/SistemaTyah/interfaces/interfacesDashboard.ts
@@ -0,0 +1,8 @@
+// Interfaces Dashboard
+export interface IDashboard {
+  titulo: string;
+  valor: string;
+  variacion: string;
+  color: string;
+  icono: string;
+}
--- src/SistemaTyah/pages/HomePage.tsx
+++ src/SistemaTyah/pages/HomePage.tsx
@@ -4,6 +4,9 @@ import { ICumpleanosClientes } from '../interfaces/interfacesClientes';
 import Toast from '../components/Toast';
 import { IApiError } from '../interfaces/interfacesApi';
 import { Card, Badge } from 'flowbite-react';
+import { getEstadisticasDashboard } from '../helpers/apiDashboard';
+import { IDashboard } from '../interfaces/interfacesDashboard';
+import { WaitScreen } from '../components/WaitScreen';
 
 export const HomePage = (): React.JSX.Element => {
   // Estado para manejar la carga de la página
@@ -11,45 +14,31 @@ export const HomePage = (): React.JSX.Element => {
     ICumpleanosClientes[]
   >([]);
 
-  // Estado para las estadísticas
-  const [estadisticas, setEstadisticas] = useState([
-    {
-      titulo: 'Clientes Nuevos',
-      valor: 120,
-      variacion: '+5% este mes',
-      color: 'blue',
-      icono: '👥',
-    },
-    {
-      titulo: 'Envíos',
-      valor: 45,
-      variacion: '+12% este mes',
-      color: 'green',
-      icono: '🚚',
-    },
-    {
-      titulo: 'Pedidos',
-      valor: 89,
-      variacion: '+8% este mes',
-      color: 'purple',
-      icono: '📦',
-    },
-    {
-      titulo: 'Ventas',
-      valor: 23000,
-      variacion: '+15% este mes',
-      color: 'red',
-      icono: '💰',
-    },
+  const [estadisticas, setEstadisticas] = useState<IDashboard[]>([]);
 
-    {
-      titulo: 'Inventario',
-      valor: 560,
-      variacion: '-2% este mes',
-      color: 'yellow',
-      icono: '📊',
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchDashboard = async (): Promise<void> => {
+      try {
+        setIsLoading(true);
+        const estadisticasDashboard = await getEstadisticasDashboard();
+        setEstadisticas(estadisticasDashboard.body);
+      } catch (error) {
+        const errorMessage =
+          (error as IApiError).message || 'Ocurrió un error desconocido';
+        Toast.fire({
+          icon: 'error',
+          title: 'Ocurrió un Error',
+          text: errorMessage,
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchDashboard();
+  }, []);
 
   // Obtener las fechas de cumpleaños de los clientes
   useEffect(() => {
@@ -89,68 +78,83 @@ export const HomePage = (): React.JSX.Element => {
   }, []);
 
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6">
-        Bienvenido al sistema de TAYH - Test
-      </h1>
+    <>
+      {isLoading && <WaitScreen message="cargando Estadisticas..." />}
+      <div className="p-6">
+        <h1 className="text-3xl font-bold mb-6">
+          Bienvenido al sistema de TAYH - Test
+        </h1>
 
-      {/* Cards de Estadísticas */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-        {estadisticas.map((estadistica, index) => (
-          <Card key={index} className="text-center">
-            <h2 className="text-xl font-semibold flex items-center justify-center gap-2">
-              <span>{estadistica.icono}</span>
-              {estadistica.titulo}
-            </h2>
-            <p className={`text-4xl font-bold text-${estadistica.color}-600`}>
-              {estadistica.valor}
-            </p>
-            <Badge
-              color={estadistica.color}
-              className="mt-2 text-xl p-[0.4rem] rounded-xl"
-            >
-              {estadistica.variacion}
-            </Badge>
-          </Card>
-        ))}
-      </div>
-
-      {/* Lista de Cumpleaños */}
-      <div className="bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-3xl font-bold mb-4">Próximos Cumpleaños</h2>
-        <ul>
-          {cumpleanosClientes.map((cliente, index) => (
-            <li
-              key={index}
-              className="flex items-center justify-between p-3 border-b last:border-b-0 hover:bg-gray-50"
-            >
-              <div>
-                <p className="text-2xl font-semibold mt-1">
-                  {cliente.nb_Cliente}
+        {/* Cards de Estadísticas */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+          {estadisticas && estadisticas.length > 0 ? (
+            estadisticas?.map((estadistica, index) => (
+              <Card key={index} className="text-center">
+                <h2 className="text-xl font-semibold flex items-center justify-center gap-2">
+                  <span>{estadistica.icono}</span>
+                  {estadistica.titulo}
+                </h2>
+                <p
+                  className={`text-4xl font-bold text-${estadistica.color}-600`}
+                >
+                  {estadistica.valor}
                 </p>
-                <p className="text-xl text-gray-600 mb-1">{cliente.mensaje}</p>
-              </div>
-              <Badge
-                color={
-                  cliente.nu_DiasParaCumpleanos === 0
-                    ? 'success'
-                    : cliente.nu_DiasParaCumpleanos >= 1 &&
-                        cliente.nu_DiasParaCumpleanos <= 3
-                      ? 'warning'
-                      : 'failure'
-                }
-                className="text-xl"
-              >
-                {cliente.nu_DiasParaCumpleanos === 0
-                  ? '¡Hoy es su cumpleaños!'
-                  : cliente.nu_DiasParaCumpleanos === 1
-                    ? 'Falta 1 día'
-                    : `Faltan ${cliente.nu_DiasParaCumpleanos} días`}
-              </Badge>
-            </li>
-          ))}
-        </ul>
+                <Badge
+                  color={estadistica.color}
+                  className="mt-2 text-xl p-[0.4rem] rounded-xl"
+                >
+                  {estadistica.variacion}% este mes
+                </Badge>
+              </Card>
+            ))
+          ) : (
+            <></>
+          )}
+        </div>
+
+        {/* Lista de Cumpleaños */}
+        {cumpleanosClientes && cumpleanosClientes.length > 0 ? (
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-3xl font-bold mb-4">Próximos Cumpleaños</h2>
+            <ul>
+              {cumpleanosClientes.map((cliente, index) => (
+                <li
+                  key={index}
+                  className="flex items-center justify-between p-3 border-b last:border-b-0 hover:bg-gray-50"
+                >
+                  <div>
+                    <p className="text-2xl font-semibold mt-1">
+                      {cliente.nb_Cliente}
+                    </p>
+                    <p className="text-xl text-gray-600 mb-1">
+                      {cliente.mensaje}
+                    </p>
+                  </div>
+                  <Badge
+                    color={
+                      cliente.nu_DiasParaCumpleanos === 0
+                        ? 'success'
+                        : cliente.nu_DiasParaCumpleanos >= 1 &&
+                            cliente.nu_DiasParaCumpleanos <= 3
+                          ? 'warning'
+                          : 'failure'
+                    }
+                    className="text-xl"
+                  >
+                    {cliente.nu_DiasParaCumpleanos === 0
+                      ? '¡Hoy es su cumpleaños!'
+                      : cliente.nu_DiasParaCumpleanos === 1
+                        ? 'Falta 1 día'
+                        : `Faltan ${cliente.nu_DiasParaCumpleanos} días`}
+                  </Badge>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
-    </div>
+    </>
   );
 };
